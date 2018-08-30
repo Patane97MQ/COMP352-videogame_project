33cc5829c7883640b702fd04cf7163db92899126
Stephen <stephen.patane@students.mq.edu.au>
2018-08-30 16:02:47 +1000

    Button collision working. Scipt added to 'Button' prefab.

---
+++ Assets/code/Button.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "red_clone")
+        {
+            Debug.Log("check");
+        }
+    }
+}

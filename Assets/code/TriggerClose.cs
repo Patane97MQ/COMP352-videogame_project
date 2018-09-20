bd32793e4557dab9cba80c57c1b961769e1dc474
Scott <ska997@hotmail.com>
2018-09-20 14:45:19 +1000

    Added triggerClosed prefab
    Added triggerClosed prefab

---
+++ Assets/code/TriggerClose.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerClose : MonoBehaviour {
+
+    public GameObject whichBarrier;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Player")
+        {
+            Debug.Log("collison exited");
+            whichBarrier.GetComponent<SpriteRenderer>().enabled = true;
+            whichBarrier.GetComponent<Collider2D>().enabled = true;
+
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+    
+    void OnCollisionExit2D(Collision2D collision)
+    {
+   
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+
+}

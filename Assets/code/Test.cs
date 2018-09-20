3bc68ff5f02a0e4ce8788f34f8fdafb1bced583d
Stephen Patane <stephen.patane@students.mq.edu.au>
2018-09-20 17:54:23 +1000

    Divided movement3 into 'Physics.cs' and 'PlayerMovement.cs'

--- Assets/code/Test.cs
+++ Assets/code/Test.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Indicator : MonoBehaviour {
-
-    Activating connect;
-
-
+public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

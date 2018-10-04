e380a6127016483a78130cb3eb1c88a6f27d19a1
Scott <ska997@hotmail.com>
2018-10-04 12:42:18 +1000

    Added Keith's button2 code, adjusted background colours.
    Added Keith's button2 code, adjusted background colours.

--- Assets/code/Button2Test.cs
+++ Assets/code/Button2Test.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 
 
-public class Button : Tagable
+public class Button2Test : Tagable
 {
     bool pressed = false;
 
     bool aOnce = false;
-    //public Activating triggering;
+    public Activating triggering;
+    public Activating triggering2;
+
     private void Start()
     {
         ChangeSprite();
@@ -17,22 +19,23 @@ public class Button : Tagable
         if (activateTags.Count == 0 || activateTags.Count != 0 && activateTags.Contains(collision.gameObject.tag))
             SetActivated(true);
 
-        //if (aOnce == false)
-        //{
-          //  triggering.GetComponent<Activating>().Activate();
-          //  aOnce = true;
-        //}
+        if (aOnce == false)
+        {
+            triggering.GetComponent<Activating>().Activate();
+            triggering2.GetComponent<Activating>().Activate();
+            aOnce = true;
+        }
         pressed = true;
         ChangeSprite();
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (activateTags.Count == 0  || activateTags.Count != 0 && activateTags.Contains(collision.gameObject.tag))
+        if (activateTags.Count == 0 || activateTags.Count != 0 && activateTags.Contains(collision.gameObject.tag))
             SetActivated(false);
         pressed = false;
         ChangeSprite();
-        }
+    }
     void ChangeSprite()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("objects/coloured/" + colour + "/button_" + (activated ? "on" : "off") + (pressed ? "_pressed" : ""), typeof(Sprite)) as Sprite;

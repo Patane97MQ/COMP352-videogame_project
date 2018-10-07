bc28ebce848d0e51d94de8daddf0b5a9ff4967f8
Stephen Patane <stephen.patane@students.mq.edu.au>
2018-10-08 00:13:47 +1100

    Redesigned artstyle of barrier.
    Made a bunch of mock designs, including an 8-cycle GIF. Will determine which will stick. For now, darker theme is chosen.

--- Assets/code/Objects/Barrier.cs
+++ Assets/code/Objects/Barrier.cs
@@ -5,13 +5,19 @@ public class Barrier : Activating
 
     public override void Activate()
     {
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        //gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        ChangeSprite(true);
         gameObject.GetComponent<Collider2D>().enabled = false;
     }
 
     public override void DeActivate()
     {
-        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        //gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        ChangeSprite(false);
         gameObject.GetComponent<Collider2D>().enabled = true;
+    }
+    void ChangeSprite(bool active)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("objects/barrier_4" + (active ? "_off" : ""), typeof(Sprite)) as Sprite;
     }
 }

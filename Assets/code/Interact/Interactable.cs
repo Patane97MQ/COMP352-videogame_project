219cef515a6c7a839a062e2935228881b6c99c28
Stephen Patane <stephen.patane@students.mq.edu.au>
2018-10-23 17:51:10 +1100

    Noticed and fixed bug with Pulling.
    Bug: Pulling a box whilst falling off an edge keeps pulling the original box if come in contact with another box. This is now fixed.

--- Assets/code/Interact/Interactable.cs
+++ Assets/code/Interact/Interactable.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour {
-
-    //public float radius = 1f;
-
     public abstract void Interact(Interactor interactor);
     public abstract void DeInteract(Interactor interactor);
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("TEST");
         Interactor interactor = collision.gameObject.GetComponent<Interactor>();
         if (interactor == null)
             return;
@@ -23,42 +19,4 @@ public abstract class Interactable : MonoBehaviour {
             return;
         interactor.RemoveInteractable(this);
     }
-
-    //public void Update()
-    //{
-    //    RaycastHit2D hit = Utilities.BoxCastHandler(gameObject, gameObject.transform.position, gameObject.GetComponent<Collider2D>().bounds.size + new Vector3(radius, radius), 0f, Vector2.up, 1f);
-
-    //    if (hit)
-    //    {
-    //        Interactor interactor = hit.collider.gameObject.GetComponent<Interactor>();
-    //        if (interactor == null)
-    //        {
-    //            Debug.Log("null");
-    //            return;
-    //        }
-    //        interactor.AddInteractable(this);
-    //    }
-    //}
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawWireCube(gameObject.transform.position, gameObject.GetComponent<Collider2D>().bounds.size + new Vector3(radius, radius));
-    //}
-
-
-    //public void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Interactor interactor = collision.gameObject.GetComponent<Interactor>();
-    //    if (interactor == null)
-    //        return;
-    //    interactor.AddInteractable(this);
-    //}
-
-    //public void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    Interactor interactor = collision.gameObject.GetComponent<Interactor>();
-    //    if (interactor == null)
-    //        return;
-    //    interactor.RemoveInteractable(this);
-    //}
 }
--- Assets/code/Interact/Interactor.cs
+++ Assets/code/Interact/Interactor.cs
@@ -32,28 +32,9 @@ public class Interactor : MonoBehaviour {
     }
     private void Update()
     {
-        //if (interactables.Count > 0)
-        //{
-        //    if (Input.GetKey("e"))
-        //    {
-        //        if (current == null)
-        //            current = interactables[0];
-        //    }
-        //    //interactables[0].Interact(this);
-        //    else
-        //    {
-        //        if (current != null)
-        //            current.DeInteract(this);
-        //        current = null;
-        //    }
-
-        //    //else
-        //    //    interactables[0].DeInteract(this);
-        //}
-
-        //if (current != null)
-        //    current.Interact(this);
-        if(interactables.Count > 0)
+        if (!interactables.Contains(current))
+            current = null;
+        if (interactables.Count > 0)
         {
             Utilities.DrawBox(interactables[0].GetComponent<Transform>().position, new Vector2(1f, 1f), Color.magenta);
             if (Input.GetKey("e"))
@@ -62,15 +43,12 @@ public class Interactor : MonoBehaviour {
                     current = interactables[0];
                 current.Interact(this);
             }
-            //interactables[0].Interact(this);
             else
             {
                 if (current != null)
                     current.DeInteract(this);
                 current = null;
             }
-            //else
-            //    interactables[0].DeInteract(this);
         }
     }
 }

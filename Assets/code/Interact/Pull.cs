4bde9a30d8cfdccc68df7f4ca332947f15990cc5
Stephen Patane <stephen.patane@students.mq.edu.au>
2018-11-07 18:02:21 +1100

    Pulling Animation bug fixed. Testing may be needed.

--- Assets/code/Interact/Pull.cs
+++ Assets/code/Interact/Pull.cs
@@ -13,11 +13,12 @@ public class Pull : Interactable{
 
         Collider2D collider = gameObject.GetComponent<Collider2D>();
         Collider2D iCollider = iGameObject.GetComponent<Collider2D>();
-        
+
         if (iGameObject.transform.position.x + iCollider.bounds.extents.x < gameObject.transform.position.x - collider.bounds.extents.x && pMovement.facingRight)
         {
             PullObject(pMovement, true);
-        } else if (iGameObject.transform.position.x - iCollider.bounds.extents.x > gameObject.transform.position.x + collider.bounds.extents.x && !pMovement.facingRight)
+        }
+        else if (iGameObject.transform.position.x - iCollider.bounds.extents.x > gameObject.transform.position.x + collider.bounds.extents.x && !pMovement.facingRight)
         {
             PullObject(pMovement, false);
         }
@@ -28,15 +29,16 @@ public class Pull : Interactable{
         Physics thisPhysics = gameObject.GetComponent<Physics>();
 
         float pMovementVelX = pMovement.velocity.x;
+        pMovement.flipSpriteX = false;
 
         // Simply saves the X direction of the pMovement object to this object (eg. of this object is to the left, direction will be -1)
         int direction = Math.Sign(thisPhysics.GetTransform().position.x - pMovement.GetTransform().position.x);
+
 
         // If pMovement is moving towards the object, do nothing.
         if (Math.Sign(pMovementVelX) == direction)
             return;
 
-        pMovement.flipSpriteX = false;
 
         pMovement.SetVelocity(new Vector2(0, pMovement.velocity.y));
         float x = thisPhysics.CalculateForce(pMovement, pMovementVelX);
--- Assets/code/PlayerMovement.cs
+++ Assets/code/PlayerMovement.cs
@@ -122,6 +122,7 @@ public class PlayerMovement : Physics {
 
     private void SpriteFlipX()
     {
+        Debug.Log(flipSpriteX);
         // Flips objects spriteX depending on moving left or right
         if (flipSpriteX)
         {

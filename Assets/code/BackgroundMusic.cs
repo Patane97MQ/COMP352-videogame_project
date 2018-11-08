389b0b390a1f87d91c1124accb5bdb618977051b
Patane97MQ <stephen.patane@students.mq.edu.au>
2018-11-08 18:23:30 +1100

    Demo build

---
+++ Assets/code/BackgroundMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    AudioSource source;
+    public AudioClip music;
+
+	void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        DontDestroyOnLoad(this.gameObject);
+        source.PlayOneShot(music);
+	}
+}

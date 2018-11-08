cc64bbb0ac8688e9dd1d29c5f727f1019e0a22ee
Scott <ska997@hotmail.com>
2018-11-08 14:17:44 +1100

    Modified menus to look better.
    Modified menus to look better.

---
+++ Assets/code/UI/LevelSelectScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void selectLevel()
+    {
+        int y = 0;
+        string x = gameObject.name;
+        if (!Int32.TryParse(x, out y))
+        {
+            y = -1;
+        }
+        debug.log(y);
+        return y;
+
+        SceneManager.loadScene(y);
+    }
+}
